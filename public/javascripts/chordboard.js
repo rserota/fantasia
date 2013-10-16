013ece2b5e80c093b30aa1fc7f5bb7f92274b06a
RU-021 <RU-021@Admins-Mac-mini-4.local>
2013-10-16 17:15:50 -0600

    initial commit

---
+++ public/javascripts/chordboard.js
@@ -0,0 +1,125 @@
+var tones = {
+	C1: new Audio('../media/sounds/C1.wav'),
+	Db1: new Audio('../media/sounds/Db1.wav'),
+	D1: new Audio('../media/sounds/D1.wav'),
+	Eb1: new Audio('../media/sounds/Eb1.wav'),
+	E1: new Audio('../media/sounds/E1.wav'),
+	F1: new Audio('../media/sounds/F1.wav'),
+	Gb1: new Audio('../media/sounds/Gb1.wav'),
+	G1: new Audio('../media/sounds/G1.wav'),
+	Ab1: new Audio('../media/sounds/Ab1.wav'),
+	A1: new Audio('../media/sounds/A1.wav'),
+	Bb1: new Audio('../media/sounds/Bb1.wav'),
+	B1: new Audio('../media/sounds/B1.wav'),
+	C2: new Audio('../media/sounds/C2.wav'),
+	Db2: new Audio('../media/sounds/Db2.wav'),
+	D2: new Audio('../media/sounds/D2.wav'),
+	Eb2: new Audio('../media/sounds/Eb2.wav'),
+	E2: new Audio('../media/sounds/E2.wav'),
+	F2: new Audio('../media/sounds/F2.wav'),
+	Gb2: new Audio('../media/sounds/Gb2.wav'),
+	G2: new Audio('../media/sounds/G2.wav'),
+	Ab2: new Audio('../media/sounds/Ab2.wav'),
+	A2: new Audio('../media/sounds/A2.wav'),
+	Bb2: new Audio('../media/sounds/Bb2.wav')
+}
+
+var chords = {
+	alpha:[tones.C1,tones.E1,tones.G1,tones.B1],
+	beta:[tones.F1,tones.A1,tones.C2,tones.E2],
+	gamma:[tones.G1,tones.B1,tones.D2,tones.F2],
+	delta:[tones.A1,tones.C2,tones.E2,tones.G2]
+}
+
+var boundChord = chords.alpha
+
+var down7=false
+var down8=false
+var down9=false
+var down0=false
+
+$(document).ready(function(){
+	$(document).keydown(function(event){
+////////// Left hand events		
+		if(event.which === 49){
+			console.log('1!')
+			$('#chords .keybound').removeClass('on')
+			$('#1').addClass('on')
+			boundChord = chords.alpha
+		}
+		if(event.which === 50){
+			console.log('2!')
+			$('#chords .keybound').removeClass('on')
+			$('#2').addClass('on')
+			boundChord = chords.beta
+		}
+		if(event.which === 51){
+			console.log('3!')
+			$('#chords .keybound').removeClass('on')
+			$('#3').addClass('on')
+			boundChord = chords.gamma
+		}
+		if(event.which === 52){
+			console.log('4!')
+			$('#chords .keybound').removeClass('on')
+			$('#4').addClass('on')
+			boundChord = chords.delta
+		}
+
+/////////// Right hand events
+		if(event.which === 55 && down7===false){
+			down7=true
+			console.log('7!')
+			$('#7').addClass('on')
+			boundChord[0].play()
+		}
+		if(event.which === 56 && down8===false){
+			down8=true
+			console.log('8!')
+			$('#8').addClass('on')
+			boundChord[1].play()
+		}
+		if(event.which === 57 && down9===false){
+			down9=true
+			console.log('9!')
+			$('#9').addClass('on')
+			boundChord[2].play()
+		}
+		if(event.which === 48 && down0===false){
+			down0=true
+			console.log('0!')
+			$('#0').addClass('on')
+			boundChord[3].play()
+		}
+	})
+	$(document).keyup(function(event){
+		if(event.which === 55){
+			console.log('7!')
+			$('#7').removeClass('on')
+			down7=false
+			boundChord[0].pause()
+			boundChord[0].currentTime = 0
+		}
+		if(event.which === 56){
+			console.log('8!')
+			$('#8').removeClass('on')
+			down8=false
+			boundChord[1].pause()
+			boundChord[1].currentTime = 0			
+		}
+		if(event.which === 57){
+			console.log('9!')
+			$('#9').removeClass('on')
+			down9=false
+			boundChord[2].pause()
+			boundChord[2].currentTime = 0
+		}
+		if(event.which === 48){
+			console.log('0!')
+			$('#0').removeClass('on')
+			down0=false
+			boundChord[3].pause()
+			boundChord[3].currentTime = 0
+		}
+	})
+})
---
+++ public/javascripts/jqstep.js
@@ -0,0 +1,132 @@
+
+var C1 = new Audio('../media/sounds/C1.wav'),
+    D1 = new Audio('../media/sounds/D1.wav'),
+    E1 = new Audio('../media/sounds/E1.wav'),
+    F1 = new Audio('../media/sounds/F1.wav'),
+    G1 = new Audio('../media/sounds/G1.wav'),
+    A1 = new Audio('../media/sounds/A1.wav'),
+    B1 = new Audio('../media/sounds/B1.wav'),
+    C2 = new Audio('../media/sounds/C2.wav'),
+    D2 = new Audio('../media/sounds/D2.wav'),
+    E2 = new Audio('../media/sounds/E2.wav'),
+    F2 = new Audio('../media/sounds/F2.wav'),
+    G2 = new Audio('../media/sounds/G2.wav'),
+    A2 = new Audio('../media/sounds/A2.wav'),
+    B2 = new Audio('../media/sounds/A2.wav')
+
+
+
+var song = {
+	numSteps : 0,
+	notesOn : [[],[],[],[],[],[],[],[],[],[],[],[],[],[]],
+	bpm : 120,
+	notes : [B2, A2, G2, F2, E2, D2, C2, B1, A1, G1, F1, E1, D1, C1],
+    noteNames : ['B', 'A', 'G', 'F', 'E', 'D', 'C', 'B', 'A', 'G', 'F', 'E', 'D', 'C'],
+	intervalID : undefined,
+	TimeoutIDs : [],
+    playing : false
+}
+$(document).ready(function(){
+    var setStepHandlers = function(){
+        $('.button.stepButton').off('click')
+    	$('.button.stepButton').on("click",function(){
+            $(this).toggleClass('on')
+            var col = $(this).index()
+            var row = $(this).parent().index()
+            song.notesOn[row][col] = !song.notesOn[row][col]
+        })
+    }
+    
+    setStepHandlers()
+
+
+/** Set up the step counter */
+	for (var i = 1; i <= 64; i++){
+		$('.stepCounter').append('<div class="stepCount button fantastic">' + i + '</div')
+	}
+    $('.stepCounter').append('<div class="button fantastic hidden">' + i + '</div')
+	$('.stepCount.button').on('click',function(){
+        if(!song.playing){
+		    $('.button.stepButton').off('click')
+		    $('.stepCount.button').removeClass('on')
+		    $(this).addClass('on')
+		    song.numSteps = $(this).index()+1
+		    $('.rowContainer').width(32 * song.numSteps + 500 + 'px')
+        }
+
+		for (var i=1; i < $('.row').length+1; i++ ){
+			while ($('.row'+i+' .button').length < song.numSteps){
+				$('.row'+i).append('<div class="button stepButton fantastic">' + song.noteNames[i-1] + '</div>')
+				song.notesOn[i-1].push(false)
+			}
+
+		}
+		while ($('.row1 .button').length > song.numSteps){
+			$('.button.stepButton:last-child').remove()
+			for (var i=1; i < $('.row').length+1; i++){
+			    song.notesOn[i-1].pop()
+		    }
+		}
+		setStepHandlers()
+	})
+/*////////////////////////**/
+
+/** Set up the bpm counter */
+    for (var i = 2; i <=360; i+=2){
+        $('.bpmCounter').append('<div class="bpmCount button fantastic">' + i + '</div')
+    }
+    $('.bpmCounter').append('<div class="button fantastic hidden">' + i + '</div')
+    $('.bpmCount.button').on('click',function(){
+        if(!song.playing){
+            $('.button.bpmButton').off('click')
+            $('.bpmCount.button').removeClass('on')
+            $(this).addClass('on')
+            song.bpm = ($(this).index()+1)*2
+        }
+    })
+
+/*///////////////////////*/
+
+/** Main step function */
+    var oneStep = function(){
+    	$('.stepButton').each(function(){
+            var $this = $(this)
+            song.TimeoutIDs.push(setTimeout(function(){
+                if ($this.hasClass('on')){
+                    $this.addClass('playing')
+                    song.notes[$this.parent().index()].currentTime = 0
+                    song.notes[$this.parent().index()].play()
+                    setTimeout(function(){
+                        $this.removeClass('playing')
+                    }, 300)
+                }
+            }, (60/song.bpm*1000)*$this.index()))
+        })
+    }
+    var step = function(){
+    	oneStep()
+       song.intervalID = setInterval(function(){
+            oneStep()
+        }, (60/song.bpm*1000)*song.numSteps)
+    }
+/*///////////////////**/
+
+
+    $('.steppingToggle').on('click',function(){
+    	$(this).toggleClass('on')
+        song.playing=!song.playing
+    	if($(this).hasClass('on')){
+    	    step()
+    	}
+    	else {
+    		clearInterval(song.intervalID)
+    		for (var i = 0; i < song.TimeoutIDs.length; i++){
+    		    clearTimeout(song.TimeoutIDs[i])
+    	    }
+    	}
+    })
+    $('.help').on('click',function(){
+            $('.feedback').toggleClass('helpText')
+    }) 
+})
+
