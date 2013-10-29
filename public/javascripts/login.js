a82888c30ce90cab6fd7d824bddd78957fdcd317
RU-021 <RU-021@Admins-Mac-mini-4.local>
2013-10-29 16:32:39 -0600

    buttons

--- public/javascripts/login.js
+++ public/javascripts/login.js
@@ -1,7 +1,38 @@
 $(document).ready(function(){
     var $signupButtons = $('#buttonContainer').find('.fantastic')
     $signupButtons.on('click', function(){
-        $signupButtons.removeClass('on')
-        $(this).addClass('on')
+        if (!$feedback.hasClass('transitioning')){
+            $signupButtons.removeClass('on')
+            $(this).addClass('on')
+        }
+    })
+
+    var $feedback = $('.feedback')
+    var $loginform = $('.form')
+
+    $('#login').on('click', function(){
+        if(!$feedback.hasClass('login') && !$feedback.hasClass('transitioning')){
+            $feedback.removeClass('signup')
+            $loginform.addClass('hidden')
+            $feedback.addClass('transitioning')
+            setTimeout(function(){
+                $feedback.addClass('login')
+                $loginform.removeClass('hidden')
+                $feedback.removeClass('transitioning')
+            }, 300)
+        }
+    })
+
+    $('#signup').on('click', function(){
+        if(!$feedback.hasClass('signup') && !$feedback.hasClass('transitioning')){
+            $feedback.removeClass('login')
+            $loginform.addClass('hidden')
+            $feedback.addClass('transitioning')
+            setTimeout(function(){
+                $feedback.addClass('signup')
+                $loginform.removeClass('hidden')
+                $feedback.removeClass('transitioning')
+            }, 300)
+        }
     })
 })
