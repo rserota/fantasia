8c5a72a5218970857f9ace600f5e5e6ae1916a16
RU-021 <RU-021@Admins-Mac-mini-4.local>
2013-10-31 15:11:15 -0600

    here you go, mike

--- public/javascripts/login.js
+++ public/javascripts/login.js
@@ -50,30 +50,45 @@ $(document).ready(function(){
         }
     })
 
-    var loginFail = function(action){
+    var loginFail = function(first, second){
         $feedback.addClass('fail')
         $loginform.addClass('blackout')
-        $('.username').attr('placeholder', action)        
-        $('.password').attr('placeholder', 'Failed')
+        $('.username').attr('placeholder', first).val('')  
+        $('.password').attr('placeholder', second).val('')
+        $('#submit').addClass('sad')
         setTimeout(function(){
             $feedback.removeClass('fail')
             $loginform.removeClass('blackout')
             $('.username').attr('placeholder', 'Username')        
             $('.password').attr('placeholder', 'Password')
+            $('#submit').removeClass('sad')
         }, 1500)
     }
+
     var postLoginData = function(){
         $.post('/login',{username : $('.username').val(), password : $('.password').val()}, function(data){
             window.location.href = data
         }).fail(function(){
-            loginFail('Login')
-            console.log('fail!')
+            loginFail('Login', 'Failed')
         })
     }
     
     var postSignupData = function(){
         $.post('/signup', {username : $('.username').val(), password : $('.password').val()}, function(data){
-
+            if (data.code || data.errors){
+                if (data.code === 11000){
+                    loginFail('Username', 'Taken')
+                }
+                else if ('username' in data.errors){
+                    loginFail('Username', 'Too Short')
+                }
+                else if ('password' in data.errors){
+                    loginFail('Password', 'Too Short')
+                }
+            }
+            else {
+                postLoginData()
+            }
         })
     }
 
@@ -90,7 +105,7 @@ $(document).ready(function(){
 
     $('#submit').on('click', function(){
         postData()
-        setTimeout(function(){$('#submit').removeClass('on')},700)
+        setTimeout(function(){$('#submit').removeClass('on')},1500)
     })
     $('.form').on('keyup', function(event){
         if (event.which === 13){
--- routes/silliness.js
+++ routes/silliness.js
@@ -3,6 +3,7 @@ exports.quotes = [
     "Help_I'm_trapped_in_a_URL_factory",
     "Too_close_for_missiles_I'm_switching_to_guns",
     "Welcome_to_the_internet",
-    "Buck_Wilderness"
+    "Buck_Wilderness",
+    "Badges?_We_don't_need_no_stinkin'_badges"
     
 ]
