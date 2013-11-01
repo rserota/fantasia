c94536e1cc126059359b5ab511a568ce0e0041dd
RU-021 <RU-021@Admins-Mac-mini-4.local>
2013-11-01 17:06:32 -0600

    lets do this

--- app.js
+++ app.js
@@ -134,6 +134,16 @@ app.get('/leaderboards/monthly', app.isAuthenticated, function(request, response
     })
 })
 
+app.get('/leaderboards/daily', app.isAuthenticated, function(request, response){
+    var first = new Date()
+    first.setHours(0)
+    first.setMinutes(0)
+    db.Score.find({date : {$gte : first}}, function(error, scores){
+        scores.sort(function(a,b){return b.score - a.score})
+        response.render('leaderboards', {history : 'Daily', scores : scores})
+    })
+})
+
 /** THIS ROUTE MUST BE LAST */
 app.get('/:quote', app.isAuthenticated, function(request, response){
     response.render('index', {time : new Date()})
